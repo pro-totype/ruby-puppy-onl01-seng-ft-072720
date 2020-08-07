9a54983a632e33b49df1cecbd97ec200674561f1
Sean B <butts.sean01@gmail.com>
2020-08-07 05:57:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,8 +8,25 @@ class Dog
   
   def self.initialize(name)
     @name = name
-
   end
+  
+    def self.all
+    @@all
+  end
+
+  def self.print_all
+    # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
+    puts @@all.map{ |dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
   
 
 end
